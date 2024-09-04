e683845edd8e1b7db21defe69ba7faf26e1cc34d
Tobias Rojas <tobiasurielrojas2004@gmail.com>
2024-09-04 01:19:02 -0300

    Arreglando errores

--- components/Card.jsx
+++ components/Card.jsx
@@ -20,7 +20,6 @@ export const Card = ({ enCarrito, image, title, description, price, handleAgrega
     return (
         <div className='product'>
             <img className='product-image' alt={title} src={image} />
-            <div className="product-info">
                 <h3 className='product-title'>{title}</h3>
                 <p className='product-desc'>{description}</p>
                 <p className='product-price'>${price}</p>
@@ -42,7 +41,6 @@ export const Card = ({ enCarrito, image, title, description, price, handleAgrega
                             Agregar al carrito
                         </button>
                 }
-            </div>
         </div>
     )
 }
